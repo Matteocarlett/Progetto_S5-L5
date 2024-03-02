08e5956c33905701bb88e5e01016f88d03490856
Matteo <matteo.3000@outlook.it>
2024-03-02 11:52:22 +0100

    correzione e implementazione di funzioni mancanti

--- Controllers/TipoViolazioneController.cs
+++ Controllers/TipoViolazioneController.cs
@@ -4,12 +4,12 @@ using Polizia.Models;
 
 namespace Polizia.Controllers
 {
-    public class TipoVIolazioneController : Controller
+    public class TipoViolazioneController : Controller
     {
         private readonly string _connectionString = "Server=DESKTOP-7PVKHJM\\SQLEXPRESS01; Database=Polizia; Integrated Security=true;TrustServerCertificate=True";
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult ListaViolazioni()
         {
             var tipoViolazioni = new List<TipoViolazione>();
 
@@ -40,6 +40,154 @@ namespace Polizia.Controllers
             }
 
             return View(tipoViolazioni);
+        }
+
+        [HttpGet]
+        public ActionResult AggiungiViolazione()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AggiungiViolazione(TipoViolazione model)
+        {
+            if (ModelState.IsValid)
+            {
+                string query = "INSERT INTO dbo.[Tipo Violazione] (Descrizione) " +
+                               "VALUES (@Descrizione)";
+
+                try
+                {
+                    using (var sqlConnection = new SqlConnection(_connectionString))
+                    {
+                        sqlConnection.Open();
+                        using (var cmd = new SqlCommand(query, sqlConnection))
+                        {
+                            cmd.Parameters.AddWithValue("@Descrizione", model.Descrizione);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    TempData["Messaggio"] = "Violazione aggiunta con successo!";
+                    return RedirectToAction("ListaViolazioni");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Errore"] = "Si è verificato un errore durante l'aggiunta della violazione: " + ex.Message;
+                }
+            }
+            else
+            {
+                TempData["Errore"] = "Il modello non è valido. Correggi gli errori e riprova.";
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EliminaViolazione(int IdViolazione)
+        {
+            try
+            {
+                using (var sqlConnection = new SqlConnection(_connectionString))
+                {
+                    sqlConnection.Open();
+                    string query = "DELETE FROM [Tipo Violazione] WHERE IdViolazione = @IdViolazione";
+
+                    using (var cmd = new SqlCommand(query, sqlConnection))
+                    {
+                        cmd.Parameters.AddWithValue("@IdViolazione", IdViolazione);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                TempData["Messaggio"] = "Violazione eliminata con successo!";
+            }
+            catch (Exception ex)
+            {
+                TempData["Errore"] = "Si è verificato un errore durante l'eliminazione della violazione: " + ex.Message;
+            }
+            return RedirectToAction("ListaViolazioni");
+        }
+
+        [HttpGet]
+        public ActionResult ModificaViolazione(int IdViolazione)
+        {
+            TipoViolazione violazione = null;
+
+            try
+            {
+                using (var sqlConnection = new SqlConnection(_connectionString))
+                {
+                    sqlConnection.Open();
+                    string query = "SELECT * FROM [Tipo Violazione] WHERE IdViolazione = @IdViolazione";
+
+                    using (var cmd = new SqlCommand(query, sqlConnection))
+                    {
+                        cmd.Parameters.AddWithValue("@IdViolazione", IdViolazione);
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                violazione = new TipoViolazione()
+                                {
+                                    IdViolazione = (int)reader["IdViolazione"],
+                                    Descrizione = reader["Descrizione"].ToString()
+                                };
+                            }
+                        }
+                    }
+                }
+
+                if (violazione == null)
+                {
+                    TempData["Errore"] = "Violazione non trovata!";
+                    return RedirectToAction("ListaViolazioni");
+                }
+
+                return View(violazione);
+            }
+            catch (Exception ex)
+            {
+                TempData["Errore"] = "Si è verificato un errore durante il recupero della violazione: " + ex.Message;
+                return RedirectToAction("ListaViolazioni");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvaModifiche(TipoViolazione violazioneModificata)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var sqlConnection = new SqlConnection(_connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query =
+                            "UPDATE [Tipo Violazione] SET " +
+                            "Descrizione = @Descrizione " +
+                            "WHERE IdViolazione = @IdViolazione";
+
+                        using (var cmd = new SqlCommand(query, sqlConnection))
+                        {
+                            cmd.Parameters.AddWithValue("@IdViolazione", violazioneModificata.IdViolazione);
+                            cmd.Parameters.AddWithValue("@Descrizione", violazioneModificata.Descrizione);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    TempData["Messaggio"] = "Violazione modificata con successo!";
+                }
+                catch (Exception ex)
+                {
+                    TempData["Errore"] = "Si è verificato un errore durante il salvataggio delle modifiche: " + ex.Message;
+                }
+            }
+            return RedirectToAction("ListaViolazioni");
         }
     }
 }
--- Controllers/VerbaleController.cs
+++ Controllers/VerbaleController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Polizia.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Polizia.Controllers
 {
@@ -9,7 +11,7 @@ namespace Polizia.Controllers
         private readonly string _connectionString = "Server=DESKTOP-7PVKHJM\\SQLEXPRESS01; Database=Polizia; Integrated Security=true;TrustServerCertificate=True";
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult ListaVerbali()
         {
             var verbali = new List<Verbale>();
 
@@ -41,10 +43,153 @@ namespace Polizia.Controllers
             }
             catch (Exception ex)
             {
-                return View("Error", ex.Message);
+                TempData["Errore"] = "Si è verificato un errore durante il recupero dei verbali: " + ex.Message;
+                return RedirectToAction("ListaVerbali");
             }
 
             return View(verbali);
+        }
+
+        [HttpGet]
+        public ActionResult AggiungiVerbale()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AggiungiVerbale(Verbale model)
+        {
+            if (ModelState.IsValid)
+            {
+                string query = "INSERT INTO dbo.Verbale (DataViolazione, IndirizzoViolazione, Nominativo_Agente, DataTrascrizioneVerbale, Importo, DecurtamentoPunti) " +
+                               "VALUES (@DataViolazione, @IndirizzoViolazione, @Nominativo_Agente, @DataTrascrizioneVerbale, @Importo, @DecurtamentoPunti)";
+
+                try
+                {
+                    using (var sqlConnection = new SqlConnection(_connectionString))
+                    {
+                        sqlConnection.Open();
+                        using (var cmd = new SqlCommand(query, sqlConnection))
+                        {
+                            cmd.Parameters.AddWithValue("@DataViolazione", model.DataViolazione);
+                            cmd.Parameters.AddWithValue("@IndirizzoViolazione", model.IndirizzoViolazione);
+                            cmd.Parameters.AddWithValue("@Nominativo_Agente", model.Nominativo_Agente);
+                            cmd.Parameters.AddWithValue("@DataTrascrizioneVerbale", model.DataTrascrizioneVerbale);
+                            cmd.Parameters.AddWithValue("@Importo", model.Importo);
+                            cmd.Parameters.AddWithValue("@DecurtamentoPunti", model.DecurtamentoPunti);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    TempData["Messaggio"] = "Verbale aggiunto con successo!";
+                    return RedirectToAction("ListaVerbali");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Errore"] = "Si è verificato un errore durante l'aggiunta del verbale: " + ex.Message;
+                }
+            }
+            else
+            {
+                TempData["Errore"] = "Il modello non è valido. Correggi gli errori e riprova.";
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult ModificaVerbale(int IdVerbale)
+        {
+            Verbale verbale = null;
+
+            try
+            {
+                using (var sqlConnection = new SqlConnection(_connectionString))
+                {
+                    sqlConnection.Open();
+                    string query = "SELECT * FROM Verbale WHERE IdVerbale = @IdVerbale";
+
+                    using (var cmd = new SqlCommand(query, sqlConnection))
+                    {
+                        cmd.Parameters.AddWithValue("@IdVerbale", IdVerbale);
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                verbale = new Verbale()
+                                {
+                                    IdVerbale = (int)reader["IdVerbale"],
+                                    DataViolazione = (DateTime)reader["DataViolazione"],
+                                    IndirizzoViolazione = reader["IndirizzoViolazione"].ToString(),
+                                    Nominativo_Agente = reader["Nominativo_Agente"].ToString(),
+                                    DataTrascrizioneVerbale = (DateTime)reader["DataTrascrizioneVerbale"],
+                                    Importo = (decimal)reader["Importo"],
+                                    DecurtamentoPunti = (int)reader["DecurtamentoPunti"]
+                                };
+                            }
+                        }
+                    }
+                }
+
+                if (verbale == null)
+                {
+                    TempData["Errore"] = "Verbale non trovato!";
+                    return RedirectToAction("ListaVerbali");
+                }
+
+                return View(verbale);
+            }
+            catch (Exception ex)
+            {
+                TempData["Errore"] = "Si è verificato un errore durante il recupero del verbale: " + ex.Message;
+                return RedirectToAction("ListaVerbali");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ModificaVerbale(Verbale verbaleModificato)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var sqlConnection = new SqlConnection(_connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query =
+                            "UPDATE Verbale SET " +
+                            "DataViolazione = @DataViolazione, " +
+                            "IndirizzoViolazione = @IndirizzoViolazione, " +
+                            "Nominativo_Agente = @Nominativo_Agente, " +
+                            "DataTrascrizioneVerbale = @DataTrascrizioneVerbale, " +
+                            "Importo = @Importo, " +
+                            "DecurtamentoPunti = @DecurtamentoPunti " +
+                            "WHERE IdVerbale = @IdVerbale";
+
+                        using (var cmd = new SqlCommand(query, sqlConnection))
+                        {
+                            cmd.Parameters.AddWithValue("@IdVerbale", verbaleModificato.IdVerbale);
+                            cmd.Parameters.AddWithValue("@DataViolazione", verbaleModificato.DataViolazione);
+                            cmd.Parameters.AddWithValue("@IndirizzoViolazione", verbaleModificato.IndirizzoViolazione);
+                            cmd.Parameters.AddWithValue("@Nominativo_Agente", verbaleModificato.Nominativo_Agente);
+                            cmd.Parameters.AddWithValue("@DataTrascrizioneVerbale", verbaleModificato.DataTrascrizioneVerbale);
+                            cmd.Parameters.AddWithValue("@Importo", verbaleModificato.Importo);
+                            cmd.Parameters.AddWithValue("@DecurtamentoPunti", verbaleModificato.DecurtamentoPunti);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    TempData["Messaggio"] = "Verbale modificato con successo!";
+                }
+                catch (Exception ex)
+                {
+                    TempData["Errore"] = "Si è verificato un errore durante il salvataggio delle modifiche: " + ex.Message;
+                }
+            }
+            return RedirectToAction("ListaVerbali");
         }
     }
 }
